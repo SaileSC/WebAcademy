1cd5cd8a58a97e698315664a64a918df565587fc
Saile <sailesantos0@gmail.com>
2024-03-10 10:46:16 -0400

    adicao do cross-env para atribuir variaveis no win e Exercicio I - Parte 5

--- Node.js/node1/index.js
+++ Node.js/node1/index.js
@@ -1,31 +1,40 @@
 const http = require("http");
 const fs = require("fs");
-require("dotenv").config();
+const dotenv = require("dotenv");
+
+//Configura o ambiente como produção ou desenvolvimento;
+dotenv.config({
+    path: `.env.${process.env.NODE_ENV}`
+});
 
 const PORT = process.env.PORT;
 
-const filesNames = [];
+let filesNames = [];
 
+
+// Ler arquivos da pasta ./public
 fs.readdir("public", (err, files) => {
-    if(err){
+    if (err) {
         console.log(err);
-    }else{
-        files.forEach((fileName)=>{
+    } else {
+        files.forEach((fileName) => {
+            //Adiciona nome de arquivos em filesNames {posicao : nome};
             filesNames.push(`${files.indexOf(fileName) + 1} : ${fileName}`);
-        })
+        });
+
+        //Cria servidor
+        const server = http.createServer((req, res) => {
+            res.writeHead(200, { "Content-Type": "text/html;charset=utf-8" });
+            filesNames.forEach((element) => {
+                res.write(`${element} <br>`);
+            });
+            res.end();
+        });
+
+
+        //Define porta de acordo com o .env.
+        server.listen(PORT, () => {
+            console.log(`Servidor rodando na porta ${PORT}`);
+        });
     }
-})
-
-
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {"Content-Type" : "text/html;charset=utf-8"});
-    filesNames.forEach((element) => {
-        res.write(`${element} <br>`);
-    })
-})
-
-
-
-server.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
-})
+});
