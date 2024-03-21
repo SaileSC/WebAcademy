4279c2f3720021be030ff9b0730c0b29dfe55912
Sailesc <sailesantos0@gmail.com>
2024-03-21 17:48:20 -0400

    atividade 3 Node.js

--- Node.js/node3/src/index.js
+++ Node.js/node3/src/index.js
@@ -2,7 +2,11 @@ import http from "http";
 import fs from "fs";
 import dotenv from "dotenv";
 import url from "url";
-import lorem from "./utils/lorem.js"
+import { LoremIpsum } from "lorem-ipsum";
+
+//import loremUtil from "./utils/lorem.js"
+
+//const LoremIpsum = require("lorem-ipsum").LoremIpsum;
 
 //Configura o ambiente como produção ou desenvolvimento;
 dotenv.config({
@@ -16,31 +20,35 @@ const server = http.createServer((req, res) => {
     const parsedUrl = url.parse(req.url, true);
     const filePath = './src/html/index.html';
     if (parsedUrl.pathname == "/") {
-        fs.readFile(filePath,"utf8", (err, data) => {
+        fs.readFile(filePath, "utf8", (err, data) => {
             if (err) {
-                res.writeHead(500, {'Content-Type': 'text/plain'});
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
                 res.end(err.message);
             } else {
-                res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
+                res.writeHead(200, { 'Content-Type': 'text/html;charset=utf-8' });
+                const qtdParagrafo = parsedUrl.query.qtdParagrafo || 0; //Armazena quantidade de paragrafos;
 
-                const qtdParagrafo = parsedUrl.query.qtdParagrafo || 0;
-
-                if(qtdParagrafo){
+                if (qtdParagrafo) {
+                    let lorem = new LoremIpsum();
+                    let paragraps = "";
+                    for (let x = 1; x <= qtdParagrafo; x++) {
+                        paragraps += `<p>${lorem.generateParagraphs(1)}</p>`
+                    }
                     let newData = data;
-                    res.write(qtdParagrafo);
-                    newData = newData.replace("{}}", "novo texto");
+                    newData = newData.replace("...", paragraps);
+
                     res.end(newData);
-                }else{
+                } else {
                     res.end(data);
                 }
             }
-        }); 
+        });
 
 
 
 
-    }else{
-        res.writeHead(404, {'Content-Type': 'text/plain'});
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
         res.end('Página não encontrada');
     }
 });
