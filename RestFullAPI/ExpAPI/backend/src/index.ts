5a5278319cd22567dc0d71523f8671be2026af00
Saile <sailesantos0@gmail.com>
2024-04-30 14:28:15 -0400

    aula sobre uuid e session

--- RestFullAPI/ExpAPI/backend/src/index.ts
+++ RestFullAPI/ExpAPI/backend/src/index.ts
@@ -4,6 +4,9 @@ import validateEnv from "./utils/validateEnv";
 import router from "./router"
 import setCookieLang from "./middlewares/setLangCookie";
 import cookieParser from "cookie-parser";
+import {v4 as uuidv4} from "uuid";
+import session from "express-session";
+
 
 dotenv.config();
 validateEnv();
@@ -12,6 +15,14 @@ const app = express()
 const PORT = process.env.PORT ?? 3333;
 
 app.use(cookieParser());
+
+app.use(session({
+    genid: () => uuidv4(),
+    secret: 'Hi9Cf#mK98',
+    resave: true,
+    saveUninitialized: true
+}))
+
 app.use(setCookieLang);
 app.use(express.json());
 
---
+++ RestFullAPI/ExpAPI/backend/src/resources/tipoUsuario/tipoUsuario.constants.ts
@@ -0,0 +1,4 @@
+export enum TiposUsuarios {
+    "ADMIN" = "0aaea678-4696-47cc-a54f-6ab380d493e6",
+    "CLIENT" = "743d52a7-646e-45bb-9c7c-d99dee5e868e"
+}
